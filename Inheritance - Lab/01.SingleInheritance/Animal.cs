757f33b2c5053a6ce0969d2f393a00de00f709a1
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-28 11:12:26 +0200

    Add files via upload

---
+++ Inheritance - Lab/01.SingleInheritance/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
