68d8a9378017b8120c14675bb115808977d6c95f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-30 12:02:03 +0200

    Add files via upload

---
+++ Interfaces and Abstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+        public override string ToString() => $"{Color} Seat {Model}";
+    }
+}
---
+++ Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; }
+        public string Color { get; }
+        public int Battery { get; }
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+        public override string ToString() => $"{Color} Tesla {Model} with {Battery} Batteries";
+    }
+}
