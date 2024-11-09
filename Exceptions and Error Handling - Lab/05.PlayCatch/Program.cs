5423995fd9ee6fe77801ce145c94f903815ce8db
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-11-09 13:21:28 +0200

    Add files via upload

---
+++ Exceptions and Error Handling - Lab/05.PlayCatch/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _05.PlayCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int exceptionCount = 0;
+            while (exceptionCount < 3)
+            {
+                string[] commandParts = Console.ReadLine().Split();
+                string command = commandParts[0];
+                try
+                {
+                    if (command == "Replace")
+                    {
+                        int index = int.Parse(commandParts[1]);
+                        int element = int.Parse(commandParts[2]);
+                        if (index < 0 || index >= numbers.Length)
+                        {
+                            throw new IndexOutOfRangeException();
+                        }
+                        numbers[index] = element;
+                    }
+                    else 
+                    if (command == "Print")
+                    {
+                        int startIndex = int.Parse(commandParts[1]);
+                        int endIndex = int.Parse(commandParts[2]);
+                        if (startIndex < 0 || endIndex >= numbers.Length || startIndex > endIndex)
+                        {
+                            throw new IndexOutOfRangeException();
+                        }
+                        Console.WriteLine(string.Join(", ", numbers.Skip(startIndex).Take(endIndex - startIndex + 1)));
+                    }
+                    else 
+                    if (command == "Show")
+                    {
+                        int index = int.Parse(commandParts[1]);
+                        if (index < 0 || index >= numbers.Length)
+                        {
+                            throw new IndexOutOfRangeException();
+                        }
+                        Console.WriteLine(numbers[index]);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exceptionCount++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exceptionCount++;
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ Exceptions and Error Handling - Lab/06.MoneyTransactions/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace _06.MoneyTransactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] accountData = Console.ReadLine().Split(',');
+            var accounts = new Dictionary<int, double>();
+            foreach (var account in accountData)
+            {
+                var accountInfo = account.Split('-');
+                int accountNumber = int.Parse(accountInfo[0]);
+                double balance = double.Parse(accountInfo[1]);
+                accounts[accountNumber] = balance;
+            }
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+                string[] commandParts = command.Split();
+                try
+                {
+                    if (commandParts[0] == "Deposit")
+                    {
+                        int accountNumber = int.Parse(commandParts[1]);
+                        double sum = double.Parse(commandParts[2]);
+                        if (accounts.ContainsKey(accountNumber))
+                        {
+                            accounts[accountNumber] += sum;
+                            Console.WriteLine($"Account {accountNumber} has new balance: {accounts[accountNumber]:F2}");
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid account!");
+                        }
+                    }
+                    else
+                    if (commandParts[0] == "Withdraw")
+                    {
+                        int accountNumber = int.Parse(commandParts[1]);
+                        double sum = double.Parse(commandParts[2]);
+                        if (accounts.ContainsKey(accountNumber))
+                        {
+                            if (accounts[accountNumber] >= sum)
+                            {
+                                accounts[accountNumber] -= sum;
+                                Console.WriteLine($"Account {accountNumber} has new balance: {accounts[accountNumber]:F2}");
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("Insufficient balance!");
+                            }
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid account!");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Invalid command!");
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+                Console.WriteLine("Enter another command");
+            }
+        }
+    }
+}
