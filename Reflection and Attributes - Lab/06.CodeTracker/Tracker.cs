c02f52e9f789a062f63bc0812b7809e0ace0b394
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-11-16 14:44:10 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (MethodInfo method in methods)
+            {
+                object[] attributes = method.GetCustomAttributes(typeof(AuthorAttribute), false);
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
