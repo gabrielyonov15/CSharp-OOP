f70c080abf0280b3e92e4f438d1deda7aaf1d927
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-11-17 15:43:51 +0200

    Add files via upload

---
+++ Reflection and Attributes - Exercise/02.ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+        public override bool IsValid(object obj)
+        {
+            if (obj is int value)
+            {
+                return value >= minValue && value <= maxValue;
+            }
+            return false;
+        }
+    }
+}
---
+++ Reflection and Attributes - Exercise/02.ValidationAttributes/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            var properties = obj.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                var attributes = property
+                    .GetCustomAttributes()
+                    .OfType<MyValidationAttribute>();
+                foreach (var attribute in attributes)
+                {
+                    var propertyValue = property.GetValue(obj);
+                    if (!attribute.IsValid(propertyValue))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
